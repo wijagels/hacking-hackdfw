5db3ce5262b0c3f41d154ef9b398023424d8a4da
Tim <thung1@binghamton.edu>
2016-04-10 17:04:03 -0400

    Implement mongodb

--- tracker/tracker.js
+++ tracker/tracker.js
@@ -1,6 +1,30 @@
 var request = require('request');
 var debug = require('debug')('dfw-tracker');
 var async = require('async');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+
+var url = 'mongodb://localhost:27017/tracker';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+var insertDocument = function(db, json, callback) {
+    //console.log(json);
+    db.collection('tracker').insertOne(json, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a document into the tracker collection.");
+        callback();
+    });
+};
+
+
+
+
+
 
 var headers = {
   referer: 'http://game.hackdfw.com/login',
@@ -25,6 +49,14 @@ var check = function() {
       debug('Server online');
       //clearInterval(timer);
       debug(body);
+
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        insertDocument(db, body, function() {
+        db.close();
+        });
+      });
+    
     } else {debug(body['error']);}
   });
 };

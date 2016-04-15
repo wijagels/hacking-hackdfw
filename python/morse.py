23ad233170c925e9ac506ff18de3d3c287c7f52f
Tim <thung1@binghamton.edu>
2016-04-15 02:07:05 -0400

    Pretty up morse again

--- python/morse.py
+++ python/morse.py
@@ -1,15 +1,17 @@
-"""
-Decodes a base64 encoded mp3 file of Morse code audio into plaintext.
-"""
+"""Decodes a base64 encoded mp3 file of Morse code audio into plaintext."""
 
-import numpy
-import wave
 import sys
 import subprocess
 import os
 import base64
+import wave
+import numpy
 
-if (len(sys.argv) is not 2):
+#######################
+# Arguments checking  #
+#######################
+
+if len(sys.argv) != 2:
     print('\n    Must have an base64 text file as the sole argument!\n')
     exit()
     
@@ -17,80 +19,84 @@ INPUT_TXT = sys.argv[1]
 TEMP_OUTPUT_MP3 = './convertedfrombase64.mp3'
 TEMP_OUTPUT_WAV = './output.wav'
 
-#   arg check
-ext_start = INPUT_TXT.rfind('.')      #   find index of last '.'
-extension = INPUT_TXT[ext_start+1:]   #   grab file extension after '.'
-if extension is 'txt':
+# arg check
+ext_start = INPUT_TXT.rfind('.')      # find index of last '.'
+extension = INPUT_TXT[ext_start+1:]   # grab file extension after '.'
+if extension != 'txt':
     print('\n    Must have an base64 text file as the sole argument!\n')
     exit()
 
 
 
-#################################
-#   Pre-processing conversions  #
-#################################
-#   decode base64 into mp3
+##############################
+# Pre-processing conversions #
+##############################
+
+# decode base64 into mp3
 with open(TEMP_OUTPUT_MP3, 'wb') as file:
     file.write(base64.b64decode(open(INPUT_TXT, 'rb').read()))
 
-#   convert mp3 to wav
+# convert mp3 to wav
 subprocess.call([
     'ffmpeg', 
-    '-loglevel', 'panic',   #   don't log crap unless it's critical
-    '-hide_banner',         #   hide stupid copyright licence
-    '-y',                   #   overwrite file
+    '-loglevel', 'panic',   # don't log crap unless it's critical
+    '-hide_banner',         # hide stupid copyright licence
+    '-y',                   # overwrite file
     '-i',                    
     TEMP_OUTPUT_MP3, 
     TEMP_OUTPUT_WAV
 ])
 os.remove(TEMP_OUTPUT_MP3)
 
-#   seed signal[] with values from the .wav file
-spf = wave.open('output.wav', 'r')
+# seed signal[] with values from the .wav file
+spf = wave.open(TEMP_OUTPUT_WAV, 'r')
 os.remove(TEMP_OUTPUT_WAV)
-signal = spf.readframes(-1)
-signal = numpy.fromstring(signal, 'Int16')
-
-#   normalize to all positives
-signal = [abs(x) for x in signal]
+signal = numpy.fromstring(spf.readframes(-1), 'Int16')
 
 
 
-########################
-#   Audio Processing   #
-########################
-'''
-    each entry of averages[] is the average of each <STEP> entries in signal
-    if the average is above a volume threshold, we assume a tone is playing
-    if a tone is playing, we set the average as 1, else as 0
-'''
+####################
+# Audio Processing #
+####################
+"""
+each entry of averages[] is the average of each <STEP> entries in signal
+if the average is above a volume threshold, we assume a tone is playing
+if a tone is playing, we set the average as 1, else as 0
+"""
 STEP = 2000
 VOLUME_THRESHOLD = 50
-averages = [0 if (sum(signal[i*STEP : (i+2)*STEP]) / STEP) < VOLUME_THRESHOLD else 1 for i in range(1, ((len(signal)//STEP) - 1))]
+
+# normalize to all positives
+signal = [abs(x) for x in signal]
+
+averages = [0 if (sum(signal[i*STEP : (i+2)*STEP]) / STEP) < VOLUME_THRESHOLD else 1 
+            for i in range(1, len(signal)//STEP - 1)]
 #print(averages)
 
-DAH_LENGTH_THRESHOLD = 7        #   Minimum length of a 'Dah' tone
-SILENCE_LENGTH_THRESHOLD = 8    #   Minimum length of letter delimiting silence
-tracker = 0                     #   Keeps track of the beginning of the last silence or sound
-code = ''                       #   Morse code transcription
+DAH_LENGTH_THRESHOLD = 7        # Minimum length of a 'Dah' tone
+SILENCE_LENGTH_THRESHOLD = 8    # Minimum length of letter delimiting silence
+tracker = 0                     # Keeps track of the beginning of the last silence or sound
+code = ''                       # Morse code transcription
 for i in range(1, len(averages)):
-    #   if average increase in volume = dit or dah
+    # if average increase in volume = dit or dah
     if averages[i] > averages[i-1]:
-        #   if silence length passes threshold, it's a full silence
-        code += ' ' if (i-tracker) > SILENCE_LENGTH_THRESHOLD else ''
+        # if silence length passes threshold, it's a full silence
+        if (i-tracker) > SILENCE_LENGTH_THRESHOLD: 
+            code += ' '
         tracker = i
-    #   if average decrease in volume = silence
+    # if average decrease in volume = silence
     elif averages[i] < averages[i-1]:
-        #   if noise length passes threshold, it's a dah. else it's a dit
+        # if noise length passes threshold, it's a dah. else it's a dit
         code += '_' if (i-tracker) > DAH_LENGTH_THRESHOLD else '.'
         tracker = i
 #print(code)
 
 
-##########################
-#   Morse to Plaintext   #
-##########################
-#   Morse lookup table
+
+######################
+# Morse to Plaintext #
+######################
+
 CODE = {'A': '._',     'B': '_...',   'C': '_._.', 
         'D': '_..',    'E': '.',      'F': '.._.',
         'G': '__.',    'H': '....',   'I': '..',
